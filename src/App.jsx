de2eb998f918b2802b8117bca1326e2694f97033
William Taylor Sockwell <wtsockwell@yahoo.com>
2021-02-28 16:17:58 -0600

    All information is correct and displaying propperly, now just working on having things display only when the buttons are clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,58 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+
+    let filmDisplay = []
 
     const [Films, setFilms] = useState([])
     const [People, setPeople] = useState([])
 
+    useEffect(() => {
+
+        fetch('https://ghibliapi.herokuapp.com/films')
+            .then(res => {
+                return res.json();
+            })
+            .then(allFilms => {
+                setFilms(allFilms);
+            })
+
+        fetch('https://ghibliapi.herokuapp.com/people')
+            .then(res => {
+                return res.json();
+            })
+            .then(allPeople => {
+                setPeople(allPeople);
+            })
+
+    }, [])
+
     const handleFilm = () => {
-        console.log("placeholder")
+       Films.map(film => (
+            <div className="col-md-6" key={`film-card-${film.id}`} >
+                <div className="card">
+                    <div className="card-body">
+                        <h3 className="card-title">{`${film.title}`}</h3>
+                        <p className="card-text">{`${film.description}`}</p>
+                    </div>
+                </div>
+            </div>
+        ))
     }
-    
+
     const handlePeople = () => {
-        console.log("placeholder")
+        People.map(person => (
+            <div className="col-md-6" key={`person-card-${person.id}`} >
+                <div className="card">
+                    <div className="card-body">
+                        <h3 className="card-title">{`${person.name}`}</h3>
+                        <p className="card-text">{`${person.age}`}</p>
+                        <p className="card-text">{`${person.gender}`}</p>
+                        <p className="card-text">{`${person.films[0]}`}</p>
+                    </div>
+                </div>
+            </div>
+        ))
     }
 
     return (
@@ -18,8 +60,14 @@ const App = () => {
             <div className="container">
                 <h1 className="text-center">Not "jib-lee"</h1>
                 <div className="d-flex justify-content-center">
-                <button className="btn btn-outline-dark m-2" onClick={handleFilm}>Load Films</button>
-                <button className="btn btn-outline-dark m-2" onClick={handlePeople}>Load People</button>
+                    <button className="btn btn-outline-dark m-2" onClick={handleFilm}>Load Films</button>
+                    <button className="btn btn-outline-dark m-2" onClick={handlePeople}>Load People</button>
+                </div>
+                <div className="row justify-content-center mt-5" id="film-container">
+                    
+                </div>
+                <div className="row justify-content-center mt-5" id="people-container">
+                    
                 </div>
             </div>
         </div>
