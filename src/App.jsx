5231f6a08f1719cbc251eaf0a385d2ad0e9ff928
William Taylor Sockwell <wtsockwell@yahoo.com>
2021-02-28 15:16:40 -0600

    Ready to start connecting with webpage and begin practicing APIs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const App = () => {
+
+    const [Films, setFilms] = useState([])
+    const [People, setPeople] = useState([])
+
+    const handleFilm = () => {
+        console.log("placeholder")
+    }
+    
+    const handlePeople = () => {
+        console.log("placeholder")
+    }
+
     return (
         <div>
-            <h1>Not "jib-lee"</h1>
+            <div className="container">
+                <h1 className="text-center">Not "jib-lee"</h1>
+                <div className="d-flex justify-content-center">
+                <button className="btn btn-outline-dark m-2" onClick={handleFilm}>Load Films</button>
+                <button className="btn btn-outline-dark m-2" onClick={handlePeople}>Load People</button>
+                </div>
+            </div>
         </div>
     )
 }
